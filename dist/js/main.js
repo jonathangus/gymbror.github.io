feebe39d477d986a758f705e94a7004e2927cee8
jonathangus <jonathan.gustafsson@wunderkraut.com>
2015-04-06 18:32:30 +0200

    update chart

--- dist/js/main.js
+++ dist/js/main.js
@@ -45609,8 +45609,8 @@ var margin = {top: 20, right: 20, bottom: 20, left: 20},
 var svg;
 
 function _GraphGoTime(items) {
-
   svg = d3.select('#chart').append("svg")
+    .attr('class', 'Graph')
     .attr("width", width)
     .attr("height", height)
     .attr('viewBox', '0 0 ' + width + ' ' + (height + margin.top + margin.bottom))
@@ -45618,8 +45618,12 @@ function _GraphGoTime(items) {
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
   _GraphGoItems(items);
+}
+
+function _GraphUpdate(items) {
+  d3.select("svg").remove();
+  _GraphGoTime(items); 
 }
 
 function _GraphGoItems(items) {
@@ -45632,10 +45636,11 @@ function _GraphGoItems(items) {
 
   data = gItems;
 
+  data.sort(function(a, b) {
+    return a.date - b.date;
+  });
+
   var parseDate = d3.time.format("%d-%b-%y").parse,
-    bisectDate = d3.bisector(function(d) {
-      return d.date;
-    }).left,
     formatValue = d3.format(",.2f");
 
   var maxY = d3.max(data, function(d) {
@@ -45653,7 +45658,7 @@ function _GraphGoItems(items) {
     .orient("bottom")
     .tickSize(0)
     .tickFormat(d3.time.format('%b %e'))
-    .ticks(d3.time.days, 1);
+    .ticks(d3.time.days, 0);
 
   var yAxis = d3.svg.axis()
     .scale(y)
@@ -45668,11 +45673,6 @@ function _GraphGoItems(items) {
     .y(function(d) {
       return y(d.close);
     });
-
-
-  data.sort(function(a, b) {
-    return a.date - b.date;
-  });
 
   x.domain([data[0].date, data[data.length - 1].date]);
   y.domain(d3.extent(data, function(d) {
@@ -45693,27 +45693,38 @@ function _GraphGoItems(items) {
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-  svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + (width - 50) + ",0)")
-    .call(yAxis);
-  d3.selectAll('.y.axis g.tick').attr("class", "yAxisTicks tick");
   d3.selectAll('.x.axis g.tick').attr("class", "xAxisTicks tick");
 
-  function make_y_axis() {
-    return d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(maxY / 1000);
+  var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .text("a simple tooltip")
+    .attr("class", "Graph-tooltip");
 
-  };
-  svg.append("g")
-    .attr("class", "grid")
-    .style("stroke-dasharray", ("3, 3"))
-    .call(make_y_axis()
-      .tickSize((-width + margin.right + margin.left), 0, 0)
-      .tickFormat("")
-    );
+  svg.selectAll("dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("r", 7.5)
+    .attr("cx", function(d) { return x(d.date); })
+    .attr("cy", function(d) { return y(d.close); })
+    .on("mouseover", function(d) {
+      tooltip.style("visibility", "visible");
+      var html = '';
+
+      d.values.forEach(function(i) {
+        html += '<div class="Graph-tooltip-row">';
+        html += '<span class="Graph-tooltip-rep">' + i.reps + ', </span>';
+        html += '<span class="Graph-tooltip-value">' + i.value + '</span>';
+        html += '</div>';
+      });
+
+      tooltip.html(html); 
+
+    })
+    .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
   // svg.append("path")
   //   .datum(data)
@@ -45734,59 +45745,6 @@ function _GraphGoItems(items) {
         .ease("linear")
         .attr("stroke-dashoffset", 0);
 
-  var focusLine = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
-
-  var focus = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
-
-  focus.append("text")
-      .attr("x", 1)
-      .attr("dy", ".05em");
-
-  var focusText = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
-
-  focus.append("circle")
-      .attr("r", 6);
-
-  focusLine.append("line")
-        .attr("class", "x")
-        .style("stroke", "#46464F")
-        .style("opacity", 0.9)
-        .attr("y1", 0)
-        .attr("y2", -maxY);
-
-  focusText.append("text")
-      .attr("x", 9)
-      .attr("dy", ".35em");
-
-  svg.append("rect")
-      .attr("class", "overlay")
-      .attr("width", width)
-      .attr("height", height)
-      .on("mouseover", function() { focus.style("display", null);        
-                                    focusLine.style("display", null)
-                                    focusText.style("display", null);;})
-      .on("mouseout", function() { focus.style("display", "none"); 
-                                   focusLine.style("display", "none");
-                                   focusText.style("display", "none");})
-      .on("mousemove", mousemove);
-
-  function mousemove() {
-  var x0 = x.invert(d3.mouse(this)[0]),
-      i = bisectDate(data, x0, 1),
-      d0 = data[i - 1],
-      d1 = data[i],
-      d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-
-  focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-  focusText.select("text").attr("transform", "translate(" + x(d.date) + "," + -10 + ")").text(d.summary + " summary");
-  // focusLine.attr("transform", "translate(" + x(d.date) + "," + height + ")");
-}
 }
 
 var Graph = React.createClass({displayName: 'Graph',
@@ -45817,7 +45775,7 @@ var Graph = React.createClass({displayName: 'Graph',
     
     ExercisesStore.getSortedWorkouts(this.props.exerciseID).then(function(items) {
       that.setState({items: items});
-      _GraphGoTime(items)
+      _GraphUpdate(items)
     });
   },
 
@@ -45826,17 +45784,9 @@ var Graph = React.createClass({displayName: 'Graph',
   },
 
   render:function(){
-
-    var graph = [];
-
-    for(key in this.state.items) {
-      graph.push(GraphItem({key: key, removeWorkout: this.removeWorkout, item: this.state.items[key]}))
-    }
-
     return (
       React.DOM.div(null, 
-      React.DOM.div({id: "chart"}), 
-      React.DOM.div(null, graph)
+      React.DOM.div({id: "chart"})
       )
     )
   }
